66354092f91f24a2ebb1df5e511875ed71b48293
nuraga123 <nuragayusifli@gmail.com>
2025-08-12 23:28:22 +0400

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+
+        <div>
+
+        <h1>
+          How does the internet work?
+        </h1>
+          <p>📡 Фундаментальное о
