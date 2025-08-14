b9e5ff8af0b8bf1c1ec67f0ae1fd4b33c24e0825
nuraga123 <nuragayusifli@gmail.com>
2025-08-14 09:57:51 +0400

    fixs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <div>home
+    <div className="flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-900">Home</h1>
     </div>
   );
 }
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -19,10 +19,10 @@ export default function MyRootLayout({
       </div>
 
       <main className="w-full min-h-screen bg-white">
-        <button onClick={() => setShow(!show)} className={`sticky top-5 ml-5 left-10 bg-blue-500 text-white  px-4 py-2 rounded-full  text-xl md:text-xl font-extrabold mb-10 cursor-pointer 
+        <button onClick={() => setShow(!show)} className={`sticky top-5 ml-5 left-10 bg-blue-500 text-white  px-4 py-2 rounded-full  font-extrabold mb-10 cursor-pointer 
     select-none transition-all duration-300 transform 
     hover:text-blue-700 hover:scale-105`}>
-          {show ? "закрыть меню" : "открыть меню"}
+          {show ? "X" : "Menu"}
         </button>
         {children}
       </main>
